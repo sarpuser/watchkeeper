a5bc5fa354eb71f9cc2f4d2965a617b916fab6f2
Sarp User <sarpuser@gmail.com>
2025-02-22 21:40:46 -0500

    added StatusResultType

--- src/watchkeeper/monitoring/status_checker.py
+++ src/watchkeeper/monitoring/status_checker.py
@@ -1,7 +1,15 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class StatusResult(ABC):
+	is_up: bool
+	timestamp: datetime
 
 
 class StatusChecker(ABC):
 	@abstractmethod
-	def check_status(self) -> dict:
+	def check_status(self) -> StatusResult:
 		pass
