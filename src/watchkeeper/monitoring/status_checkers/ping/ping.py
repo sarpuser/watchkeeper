4c74dcdc673f1ffac451f3d74c84e33e78459570
Sarp User <sarpuser@gmail.com>
2025-03-06 18:18:05 -0500

    feat(ping): ping parser complete

---
+++ src/watchkeeper/monitoring/status_checkers/ping/ping.py
@@ -0,0 +1,117 @@
+import re
+import subprocess  # nosec B404
+from dataclasses import KW_ONLY, dataclass
+from datetime import datetime
+from enum import Enum
+
+from ....utils.ip_address import IPAddress
+
+
+class PingStatus(Enum):
+	SUCCESS = 0
+	TIMEOUT = 1
+	UNKNOWN_HOST = 2
+	PARSE_ERROR = 3
+	EXECUTION_ERROR = 4
+	NETWORK_ERROR = 5
+
+
+@dataclass
+class PingResult:
+	hostname: str
+	ip_address: IPAddress | None
+	_: KW_ONLY
+	packets_sent: int = 0
+	packets_received: int = 0
+	packet_loss: float = 0
+	rtt_min: float = 0
+	rtt_avg: float = 0
+	rtt_max: float = 0
+	rtt_std_dev: float = 0
+	error: PingStatus
+	__timestamp: datetime = None
+
+	def __post_init__(self) -> None:
+		self.__timestamp = datetime.now()
+
+
+def ping(address: str, *, icmp_count: int = 1, timeout: int = 1) -> PingResult:
+	try:
+		process_result = _execute_ping_command(address, icmp_count, timeout)
+		return _parse_output(process_result)
+	except subprocess.SubprocessError:
+		return PingResult(address, None, error=PingStatus.EXECUTION_ERROR)
+
+
+def _execute_ping_command(
+	address: str, icmp_count: int = 1, timeout: int = 1
+) -> subprocess.CompletedProcess:
+	if icmp_count < 1:
+		raise ValueError("ping: count of packets to transmit must be greater than 1")
+	if timeout < 1:
+		raise ValueError("ping: timeout must be greater than 1")
+	return None
+
+
+def _parse_output(process_result: subprocess.CompletedProcess) -> PingResult:
+	stdout = process_result.stdout
+	address = process_result.args[-1]
+	if "Unknown host" in stdout:
+		return PingResult(address, None, error=PingStatus.UNKNOWN_HOST)
+
+	output_lines = stdout[:-1].split("\n")
+
+	ip_address_match = re.search(r"((\d{1,3}\.){3}\d{1,3})", output_lines[0])
+	if ip_address_match is None:
+		return PingResult(address, None, error=PingStatus.PARSE_ERROR)
+
+	ip_address = IPAddress(ip_address_match[1])
+
+	packet_summary = (
+		output_lines[-2] if "min/avg/max" in output_lines[-1] else output_lines[-1]
+	)
+
+	packets_sent = int(re.search(r"(\d+) packets transmitted", packet_summary)[1])
+	packets_received = int(re.search(r"(\d+) packets received", packet_summary)[1])
+	packet_loss = float(re.search(r"([\d\.]+)% packet loss", packet_summary)[1])
+
+	if "No route to host" in output_lines[1]:
+		return PingResult(
+			address,
+			ip_address,
+			packets_sent=packets_sent,
+			packets_received=packets_received,
+			packet_loss=packet_loss,
+			error=PingStatus.NETWORK_ERROR,
+		)
+
+	if packets_received == 0:
+		return PingResult(
+			address,
+			ip_address,
+			packets_sent=packets_sent,
+			packets_received=packets_received,
+			packet_loss=packet_loss,
+			error=PingStatus.TIMEOUT,
+		)
+
+	rtt_summary = output_lines[-1]
+	rtt_values = re.search(r" (([\d\.]+/){3}[\d\.]+) ms", rtt_summary)[1].split("/")
+	rtt_min = float(rtt_values[0])
+	rtt_avg = float(rtt_values[1])
+	rtt_max = float(rtt_values[2])
+	rtt_std_dev = float(rtt_values[3])
+	error = PingStatus.SUCCESS
+
+	return PingResult(
+		address,
+		ip_address,
+		packets_sent=packets_sent,
+		packets_received=packets_received,
+		packet_loss=packet_loss,
+		rtt_min=rtt_min,
+		rtt_avg=rtt_avg,
+		rtt_max=rtt_max,
+		rtt_std_dev=rtt_std_dev,
+		error=error,
+	)
